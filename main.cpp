3b20f7e753b7c0ab7f3596a51a9d0fd6cae6b93d
Vinicius Priori <>
2021-05-21 03:33:44 +0000

    funcionando

--- main.cpp
+++ main.cpp
@@ -3,39 +3,50 @@
 
 using namespace std;
 
+int trocaP(char *m, char s1, char s2, char *m2){
+    int cont = 0;
+
+    while(*m2 != '\0'){
+        if(*m2==s1){
+          *m2=s2;
+          cont++;  
+        }
+        cout << *m2; 
+        m2++;
+        
+    }
+      cout << endl;
+
+    while(*m != '\0'){
+        cout << *m; 
+        m++;
+    }
+
+    return cont;
+}
+
 int main() {
-  char mensagem[10]; 
+  char mensagem[50]; 
   int num = 0; 
   char* m = mensagem;
   char s1, s2;
-  char novamen[10];
-  char *m2 = novamen; 
+  char novamen[50];
+  char* m2 = novamen; 
 
   cout << "digite mensagem: \n";
   cin >> mensagem;
 
-  //cout << "caractere a ser substituido: \n";
-  cin >> s1; 
-  m=&s1; 
+  cout << "caractere a ser substituido: \n";
+  cin >> s1;
 
-  //cout << "caractere a ser colocado no lugar do anterior: \n";
-  cin >> s2; 
-  m2=&s2; 
+  cout << "caractere a ser colocado no lugar do anterior: \n";
+  cin >> s2;   
 
-  for(m=mensagem; *m!='\0'; m++){
-        if(*m==s1){
-          m2=m;
-        }
-        cout << *m;
+  for(int i =0; i<size(mensagem); i++){
+      novamen[i] = mensagem[i];
   }
 
-  cout << endl; 
-
-    for(m2=mensagem; *m2!='\0'; m2++){
-        if(*m2==s1){
-          *m2=s2;
-        }
-        cout << *m2;
-  }
+  num = trocaP(m, s1, s2, m2);
+  cout << endl << num; 
 
 }
