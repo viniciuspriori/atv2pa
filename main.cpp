ed47dfc0fb3fc787130f8c125e0b1f3f77f5beb1
Vinicius Priori <>
2021-05-21 01:40:15 +0000

    for iterando com ponteiro

--- main.cpp
+++ main.cpp
@@ -4,23 +4,30 @@
 using namespace std;
 
 int main() {
-  char mensagem[100]; 
+  char mensagem[10]; 
   int num; 
   char* m = mensagem;
   char s1, s2;
+  char novamen[10];
+  char *m2 = novamen; 
 
   cout << "digite mensagem: \n";
   cin >> mensagem;
 
-  m = mensagem;
-
-  cout << *m << endl; 
-
-  cout << "caractere a ser substituido: \n";
+  //cout << "caractere a ser substituido: \n";
   cin >> s1; 
+  m=&s1; 
 
-  cout << "caractere a ser colocado no lugar do anterior: \n";
+  //cout << "caractere a ser colocado no lugar do anterior: \n";
   cin >> s2; 
+  m2=&s2; 
 
+  for(m=mensagem; *m!='\0'; m++){
+        cout << *m;
+       /* if(mensagem[i]==s1){
+          m=&mensagem[i];
+        }*/
+
+  }
 
 }
